10257e7e25a83509a53c593d99fbe603a25369a6
schrap <schellander.raphael@hotmail.de>
2020-11-12 11:33:46 +0100

    add script for enemy health

--- 2D-Platformer/Assets/Scripts/Enemies/EnemyDamage.cs
+++ 2D-Platformer/Assets/Scripts/Enemies/EnemyDamage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damage : MonoBehaviour
+public class EnemyDamage : MonoBehaviour
 {
 
     public Transform player;
---
+++ 2D-Platformer/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public int health = 5;
+
+    private int m_Health;
+
+
+    void Start()
+    {
+        m_Health = health;
+    }
+
+    public void receiveDamage(int damage)
+    {
+        m_Health -= damage;
+
+        if (m_Health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
