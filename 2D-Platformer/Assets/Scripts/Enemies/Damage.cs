6dcfb4654823ff2119aa4d9fbd4d23e915ef975c
schrap <schellander.raphael@hotmail.de>
2020-11-12 08:29:51 +0100

    add script Damage that allows an enemie to deal damage to player

---
+++ 2D-Platformer/Assets/Scripts/Enemies/Damage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+
+    public Transform player;
+    public int damageDealt;
+
+    private PlayerHealth m_PlayerHealth;
+
+    private void Start()
+    {
+        m_PlayerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (player.GetComponent<Collider2D>().Equals(collision))
+        {
+            m_PlayerHealth.receiveDamage(damageDealt);
+        }
+    }
+
+}
+
